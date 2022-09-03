ee4c1c3d351f665f6b69581fafbbc3f0809ff9e7
William <William.Jeffery.Knight@protonmail.com>
2022-09-03 09:31:25 -0400

    set static ip in baseUrl.js

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,3 +1,5 @@
-// export const baseUrl = 'http://192.168.1.6:3001/';
+// static ip wifi
+export const baseUrl = 'http://192.168.1.6:3001/';
 
-export const baseUrl = 'http://192.168.43.194:3001/';
+//static ip for wire
+// export const baseUrl = 'http://192.168.43.194:3001/';

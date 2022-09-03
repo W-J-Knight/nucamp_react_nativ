c9e63a2e33b017245562ca70855795582b578941
William <William.Jeffery.Knight@protonmail.com>
2022-09-03 10:51:51 -0400

    finish task one

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,13 +1,19 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
-import { useSelector, useDispatch } from 'react-redux';
-import RenderCampsite from '../features/campsites/RenderCampsite';
-import { toggleFavorite } from '../features/favorites/favoritesSlice';
+import { useState } from "react";
+import { FlatList, StyleSheet, Text, View, Button, Modal } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import RenderCampsite from "../features/campsites/RenderCampsite";
+import { toggleFavorite } from "../features/favorites/favoritesSlice";
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
     const comments = useSelector((state) => state.comments);
     const favorites = useSelector((state) => state.favorites);
     const dispatch = useDispatch();
+    const [showModal, setShowModal] = useState(false);
+
+    const handleSubmit = () => {
+        // setShowModal(!showModal);
+    };
 
     const renderCommentItem = ({ item }) => {
         return (
@@ -22,45 +28,72 @@ const CampsiteInfoScreen = ({ route }) => {
     };
 
     return (
-        <FlatList
-            data={comments.commentsArray.filter(
-                (comment) => comment.campsiteId === campsite.id
-            )}
-            renderItem={renderCommentItem}
-            keyExtractor={(item) => item.id.toString()}
-            contentContainerStyle={{
-                marginHorizontal: 20,
-                paddingVertical: 20
-            }}
-            ListHeaderComponent={
-                <>
-                    <RenderCampsite
-                        campsite={campsite}
-                        isFavorite={favorites.includes(campsite.id)}
-                        markFavorite={() => dispatch(toggleFavorite(campsite.id))}
-                    />
-                    <Text style={styles.commentsTitle}>Comments</Text>
-                </>
-            }
-        />
+        <>
+            <FlatList
+                data={comments.commentsArray.filter(
+                    (comment) => comment.campsiteId === campsite.id
+                )}
+                renderItem={renderCommentItem}
+                keyExtractor={(item) => item.id.toString()}
+                contentContainerStyle={{
+                    marginHorizontal: 20,
+                    paddingVertical: 20,
+                }}
+                ListHeaderComponent={
+                    <>
+                        <RenderCampsite
+                            campsite={campsite}
+                            isFavorite={favorites.includes(campsite.id)}
+                            markFavorite={() =>
+                                dispatch(toggleFavorite(campsite.id))
+                            }
+                            onShowModal={() => setShowModal(!showModal)}
+                        />
+                        <Text style={styles.commentsTitle}>Comments</Text>
+                    </>
+                }
+            />
+            <Modal
+                animationType="slide"
+                transparent={false}
+                visible={showModal}
+                onRequestClose={() => setShowModal(!showModal)}
+            >
+                <View style={styles.modal}>
+                    <View style={{ margin: 10 }}>
+                        <Button
+                            onPress={() => {
+                                setShowModal(!showModal);
+                            }}
+                            color="#808080"
+                            title="Cancel"
+                        ></Button>
+                    </View>
+                </View>
+            </Modal>
+        </>
     );
 };
 
 const styles = StyleSheet.create({
     commentsTitle: {
-        textAlign: 'center',
-        backgroundColor: '#fff',
+        textAlign: "center",
+        backgroundColor: "#fff",
         fontSize: 16,
-        fontWeight: 'bold',
-        color: '#43484D',
+        fontWeight: "bold",
+        color: "#43484D",
         padding: 10,
-        paddingTop: 30
+        paddingTop: 30,
     },
     commentItem: {
         paddingVertical: 10,
         paddingHorizontal: 20,
-        backgroundColor: '#fff'
-    }
+        backgroundColor: "#fff",
+    },
+    modal: {
+        justifyContent: "center",
+        margin: 20,
+    },
 });
 
 export default CampsiteInfoScreen;
